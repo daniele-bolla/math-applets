023bdb3e4f717f14be3fdf31e6797ccab709ae40
Daniele Bolla <bolladaniele@gmail.com>
2025-11-07 17:31:26 +0000

    add sidebar responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     site: 'https://daniele-bolla.github.io',
-    base: '/math-applets/',
+    base: '/math-applets',
     vite: {
         plugins: [tailwindcss()],
     },
