768f071fd99d3bfbf5bf84ef76f70b5443c4a632
Daniele Bolla <bolladaniele@gmail.com>
2025-11-10 19:17:00 +0000

    adds read me and linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true, node: true },
+
+  extends: [
+    "eslint:recommended",
+    "plugin:astro/recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier"
+  ],
+
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+
+  plugins: ["react", "@typescript-eslint"],
+
+  rules: {
+    "react/react-in-jsx-scope": "off",  
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "no-undef": "off"
+  }
+};
