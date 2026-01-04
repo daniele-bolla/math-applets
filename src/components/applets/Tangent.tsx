5bae13e3dabcc96f1cf0402247d2c9702ab0f554
Daniele Bolla <bolladaniele@gmail.com>
2026-01-04 10:32:15 +0000

    refactored tangent example to use helper functions

--- src/components/applets/Tangent.tsx
+++ src/components/applets/Tangent.tsx
@@ -1,6 +1,17 @@
 import JSXGraphBoard from "../JSXGraphBoard";
 import JXG from "jsxgraph";
-import { COLORS, DEFAULT_GLIDER_ATTRIBUTES, DEFAULT_POINT_ATTRIBUTES } from "../../utils/jsxgraph";
+import {
+    COLORS,
+    createFunctionGraph,
+    createText,
+    createGlider,
+    createPoint,
+    createSecant,
+    createDashedSegment,
+    createArrowSegment,
+    createSlider,
+    createTangent
+} from "../../utils/jsxgraph";
 
 export default function TangentApplet() {
     return (
@@ -11,152 +22,110 @@ export default function TangentApplet() {
                 const f = (x: number) => x * x;
                 const fPrime = (x: number) => 2 * x;
 
-                const graph = board.create('functiongraph', [f, -10, 10], {
-                    strokeColor: COLORS.blue,
-                    strokeWidth: 3,
-                });
+                const graph = createFunctionGraph(board, f, [-10, 10]);
 
-                board.create('text', [3.5, f(3.5) + 0.3, 'f(x) = x²'], {
-                    fontSize: 16,
-                    color: COLORS.blue,
+                createText(board, [3.5, f(3.5) + 0.3], 'f(x) = x²', {
                     fixed: true,
                 });
 
-                const pointA = board.create('glider', [0.5, f(0.5), graph], {
-                    ...DEFAULT_GLIDER_ATTRIBUTES,
+                const pointA = createGlider(board, [0.5, f(0.5), graph], {
                     name: '(a, f(a))',
-                    face: '<>',
-                    size: 6,
-                    fillColor: COLORS.green,
-                    strokeColor: COLORS.darkGreen,
-                });
+                }, COLORS.green);
 
-
-                const hSlider = board.create('slider', [
+                const hSlider = createSlider(board,
                     [0.5, -0.5],
                     [3, -0.5],
-                    [0.001, 1, 3]
-                ], {
-                    name: 'h',
-                    snapWidth: 0.01,
-                });
+                    [0.001, 1, 3],
+                    { name: 'h' }
+                );
 
                 // Point B = A + h
-                const pointB = board.create('point', [
-                    () => pointA.X() + hSlider.Value(),
-                    () => f(pointA.X() + hSlider.Value())
-                ], {
-                    ...DEFAULT_POINT_ATTRIBUTES,
-                    name: '(a + h, f(a + h))',
-                    size: 2,
-                    fillColor: COLORS.red,
-                    strokeColor: COLORS.darkRed,
-                    fixed: true,
-                    visible: () => hSlider.Value() >= 0.1
-                });
-
+                const pointB = createPoint(board,
+                    [() => pointA.X() + hSlider.Value(), () => f(pointA.X() + hSlider.Value())],
+                    {
+                        name: '(a + h, f(a + h))',
+                        fixed: true,
+                        visible: () => hSlider.Value() >= 0.1
+                    },
+                    COLORS.red
+                );
 
                 // Secant line through A and B
-                board.create('line', [pointA, pointB], {
-                    strokeColor: COLORS.red,
-                    strokeWidth: 3,
-                    dash: 1,
-                    straightFirst: true,
-                    straightLast: true,
+                createSecant(board, [pointA, pointB], {
                     visible: () => hSlider.Value() >= 0.1
-
-                });
+                }, COLORS.red);
 
                 // Secant line label
-                board.create('text', [
-                    () => (pointA.X() + pointB.X()) / 2 + 0.2,
-                    () => (pointA.Y() + pointB.Y()) / 2 + 0.3,
-                    () => `Secant: slope = ${((pointB.Y() - pointA.Y()) / (hSlider.Value())).toFixed(3)}`
-                ], {
-                    fontSize: 12,
-                    color: COLORS.red,
-                    fixed: true,
-                    // visible: () => hSlider.Value() >= 0.1
-                });
+                createText(board,
+                    [() => (pointA.X() + pointB.X()) / 2 + 0.2, () => (pointA.Y() + pointB.Y()) / 2 + 0.3],
+                    () => `Secant: slope = ${((pointB.Y() - pointA.Y()) / (hSlider.Value())).toFixed(3)}`,
+                    {
+                        fixed: true,
+                    },
+                    COLORS.red
+                );
 
                 // Tangent line at A
-                board.create('line', [
-                    pointA,
-                    [() => pointA.X() + 1, () => pointA.Y() + fPrime(pointA.X())]
-                ], {
-                    strokeColor: COLORS.purple,
-                    strokeWidth: 3,
-                    straightFirst: true,
-                    straightLast: true,
-                    visible: () => hSlider.Value() < 0.1,
-                });
+                const tangentPoint = createPoint(board,
+                    [() => pointA.X() + 1, () => pointA.Y() + fPrime(pointA.X())],
+                    { visible: false }
+                );
 
-                board.create('text', [
-                    () => pointA.X() + 0.5,
-                    () => pointA.Y() + fPrime(pointA.X()) * 0.5 + 0.3,
-                    () => `Tangent: f'(${pointA.X()}) = ${fPrime(pointA.X()).toFixed(3)}`
-                ], {
-                    fontSize: 12,
-                    color: COLORS.purple,
-                    fixed: true,
+                createTangent(board, [pointA, tangentPoint], {
                     visible: () => hSlider.Value() < 0.1
-                });
+                }, COLORS.purple);
 
-                board.create('segment', [
-                    [() => pointA.X(), 0],
-                    pointA
-                ], {
-                    strokeColor: COLORS.green,
-                    strokeWidth: 1,
-                    dash: 2,
-                });
+                createText(board,
+                    [() => pointA.X() + 0.5, () => pointA.Y() + fPrime(pointA.X()) * 0.5 + 0.3],
+                    () => `Tangent: f'(${pointA.X()}) = ${fPrime(pointA.X()).toFixed(3)}`,
+                    {
+                        fontSize: 12,
+                        fixed: true,
+                        visible: () => hSlider.Value() < 0.1
+                    },
+                    COLORS.purple
+                );
 
-                board.create('segment', [
-                    [() => pointB.X(), 0],
-                    pointB
-                ], {
-                    strokeColor: COLORS.red,
-                    strokeWidth: 1,
-                    dash: 2,
-                });
+                // Dashed segments
+                createDashedSegment(board,
+                    [[() => pointA.X(), () => 0], pointA],
+                    {},
+                    COLORS.green
+                );
 
-                board.create('segment', [
-                    [() => pointA.X(), 0],
-                    [() => pointB.X(), 0]
-                ], {
-                    strokeColor: COLORS.orange,
-                    strokeWidth: 2,
-                    lastArrow: true,
-                });
+                createDashedSegment(board,
+                    [[() => pointB.X(), () => 0], pointB],
+                    {},
+                    COLORS.red
+                );
 
-                board.create('text', [
-                    () => (pointA.X() + pointB.X()) / 2,
-                    -0.25,
-                    () => `h = ${hSlider.Value().toFixed(3)}`
-                ], {
-                    fontSize: 12,
-                    color: COLORS.orange,
-                    fixed: true,
-                });
+                createArrowSegment(board,
+                    [[() => pointA.X(), () => 0], [() => pointB.X(), () => 0]],
+                    {},
+                    COLORS.orange
+                );
 
-                board.create('segment', [
-                    [() => pointB.X(), () => pointA.Y()],
-                    pointB
-                ], {
-                    strokeColor: COLORS.orange,
-                    strokeWidth: 2,
-                    lastArrow: true,
-                });
+                createText(board,
+                    [() => (pointA.X() + pointB.X()) / 2, () => -0.25],
+                    () => `h = ${hSlider.Value().toFixed(3)}`,
+                    {
+                    },
+                    COLORS.orange
+                );
 
-                board.create('text', [
-                    () => pointB.X() + 0.15,
-                    () => (pointA.Y() + pointB.Y()) / 2,
-                    () => `Δf = ${(pointB.Y() - pointA.Y()).toFixed(3)}`
-                ], {
-                    fontSize: 12,
-                    color: COLORS.orange,
-                    fixed: true,
-                });
+                createArrowSegment(board,
+                    [[() => pointB.X(), () => pointA.Y()], pointB],
+                    {},
+                    COLORS.orange
+                );
+
+                createText(board,
+                    [() => pointB.X() + 0.15, () => (pointA.Y() + pointB.Y()) / 2],
+                    () => `Δf = ${(pointB.Y() - pointA.Y()).toFixed(3)}`,
+                    {
+                    },
+                    COLORS.orange
+                );
             }}
         />
     );
--- src/utils/jsxgraph.ts
+++ src/utils/jsxgraph.ts
@@ -39,29 +39,215 @@ export const COLORS = {
 export const DEFAULT_GLIDER_ATTRIBUTES: JXG.GliderAttributes = {
     face: "<>",
     size: 6,
-    fillColor: COLORS.orange,
-    strokeColor: COLORS.darkOrange,
     strokeWidth: 2,
 };
 
 export const DEFAULT_POINT_ATTRIBUTES: JXG.PointAttributes = {
     size: 1,
-    fillColor: COLORS.black,
-    strokeColor: COLORS.black,
     strokeWidth: 1,
 };
 
 export const DEFAULT_LINE_ATTRIBUTES: JXG.LineAttributes = {
-    strokeColor: COLORS.blue,
     strokeWidth: 2,
 };
 
 export const DEFAULT_SEGMENT_ATTRIBUTES: JXG.SegmentAttributes = {
-    strokeColor: COLORS.blue,
     strokeWidth: 2,
 };
 
 export const DEFAULT_FUNCTION_GRAPH_ATTRIBUTES: JXG.FunctiongraphAttributes = {
     strokeColor: COLORS.blue,
-    strokeWidth: 2,
+    strokeWidth: 3,
 };
+
+export const DEFAULT_SECANT_ATTRIBUTES: JXG.LineAttributes = {
+    strokeColor: COLORS.red,
+    strokeWidth: 2,
+    dash: 2,
+    straightFirst: true,
+    straightLast: true,
+};
+
+export const DEFAULT_TANGENT_ATTRIBUTES: JXG.LineAttributes = {
+    strokeColor: COLORS.purple,
+    strokeWidth: 3,
+    straightFirst: true,
+    straightLast: true,
+};
+
+export const DEFAULT_DASHED_SEGMENT_ATTRIBUTES: JXG.SegmentAttributes = {
+    strokeColor: COLORS.gray,
+    strokeWidth: 1,
+    dash: 2,
+};
+
+export const DEFAULT_ARROW_SEGMENT_ATTRIBUTES: JXG.SegmentAttributes = {
+    strokeColor: COLORS.orange,
+    strokeWidth: 2,
+    lastArrow: true,
+};
+
+export const DEFAULT_TEXT_ATTRIBUTES: JXG.TextAttributes = {
+    fontSize: 12,
+    color: COLORS.black,
+    fixed: true,
+};
+
+export const DEFAULT_SLIDER_ATTRIBUTES: JXG.SliderAttributes = {
+    name: 'slider',
+    snapWidth: 0.01,
+};
+
+// Helper functions to create JSXGraph elements with default styles
+export function createFunctionGraph(
+    board: JXG.Board,
+    f: (x: number) => number,
+    interval: [number, number],
+    attributes: Partial<JXG.FunctiongraphAttributes> = {},
+    color: string = COLORS.blue
+) {
+    return board.create('functiongraph', [f, ...interval], {
+        ...DEFAULT_FUNCTION_GRAPH_ATTRIBUTES,
+        strokeColor: color,
+        ...attributes,
+    });
+}
+
+export function createPoint(
+    board: JXG.Board,
+    coords: [number, number] | [() => number, () => number],
+    attributes: Partial<JXG.PointAttributes> = {},
+    color: string = COLORS.black
+) {
+    const point = board.create('point', coords, {
+        ...DEFAULT_POINT_ATTRIBUTES,
+        ...attributes,
+    });
+    point.setAttribute({strokeColor: color, fillColor: color});
+    return point;
+}
+
+export function createGlider(
+    board: JXG.Board,
+    coords: [number, number, JXG.GeometryElement],
+    attributes: Partial<JXG.GliderAttributes> = {},
+    color: string = COLORS.orange
+) {
+    return board.create('glider', coords, {
+        ...DEFAULT_GLIDER_ATTRIBUTES,
+        strokeColor: color,
+        fillColor: color,
+        ...attributes,
+    });
+}
+
+export function createLine(
+    board: JXG.Board,
+    points: [JXG.Point, JXG.Point],
+    attributes: Partial<JXG.LineAttributes> = {},
+    color: string = COLORS.blue
+) {
+    const line = board.create('line', points, {
+        ...DEFAULT_LINE_ATTRIBUTES,
+        ...attributes,
+    });
+    line.setAttribute({strokeColor: color});
+    return line;
+}
+
+export function createSegment(
+    board: JXG.Board,
+    points: [JXG.Point | [number, number] | [() => number, () => number], JXG.Point | [number, number] | [() => number, () => number]],
+    attributes: Partial<JXG.SegmentAttributes> = {},
+    color: string = COLORS.blue
+) {
+    const segment = board.create('segment', points, {
+        ...DEFAULT_SEGMENT_ATTRIBUTES,
+        ...attributes,
+    });
+    segment.setAttribute({strokeColor: color});
+    return segment;
+}
+
+export function createSecant(
+    board: JXG.Board,
+    points: [JXG.Point, JXG.Point],
+    attributes: Partial<JXG.LineAttributes> = {},
+    color: string = COLORS.red
+) {
+    const line = board.create('line', points, {
+        ...DEFAULT_SECANT_ATTRIBUTES,
+        ...attributes,
+    });
+    line.setAttribute({strokeColor: color});
+    return line;
+}
+
+export function createTangent(
+    board: JXG.Board,
+    points: [JXG.Point, JXG.Point],
+    attributes: Partial<JXG.LineAttributes> = {},
+    color: string = COLORS.purple
+) {
+    const line = board.create('line', points, {
+        ...DEFAULT_TANGENT_ATTRIBUTES,
+        ...attributes,
+    });
+    line.setAttribute({strokeColor: color});
+    return line;
+}
+
+export function createText(
+    board: JXG.Board,
+    coords: [number, number] | [() => number, () => number],
+    content: string | (() => string),
+    attributes: Partial<JXG.TextAttributes> = {},
+    color: string = COLORS.black
+) {
+    const text = board.create('text', [coords[0], coords[1], content], {
+        ...DEFAULT_TEXT_ATTRIBUTES,
+        ...attributes,
+    });
+    text.setAttribute({color: color});
+    return text;
+}
+
+export function createSlider(
+    board: JXG.Board,
+    startCoords: [number, number],
+    endCoords: [number, number],
+    range: [number, number, number],
+    attributes: Partial<JXG.SliderAttributes> = {}
+) {
+    return board.create('slider', [startCoords, endCoords, range], {
+        ...DEFAULT_SLIDER_ATTRIBUTES,
+        ...attributes,
+    });
+}
+
+export function createDashedSegment(
+    board: JXG.Board,
+    points: [JXG.Point | [number, number] | [() => number, () => number], JXG.Point | [number, number] | [() => number, () => number]],
+    attributes: Partial<JXG.SegmentAttributes> = {},
+    color: string = COLORS.gray
+) {
+    return board.create('segment', points, {
+        ...DEFAULT_DASHED_SEGMENT_ATTRIBUTES,
+        strokeColor: color,
+        ...attributes,
+    });
+}
+
+export function createArrowSegment(
+    board: JXG.Board,
+    points: [JXG.Point | [number, number] | [() => number, () => number], JXG.Point | [number, number] | [() => number, () => number]],
+    attributes: Partial<JXG.SegmentAttributes> = {},
+    color: string = COLORS.orange
+) {
+    const segment = board.create('segment', points, {
+        ...DEFAULT_ARROW_SEGMENT_ATTRIBUTES,
+        ...attributes,
+    });
+    segment.setAttribute({strokeColor: color});
+    return segment;
+}
